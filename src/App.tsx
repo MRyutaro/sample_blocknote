ea3376f49222c8acf24300046c3a6d6cfb32dd6b
MRyutaro <m2002r1028@icloud.com>
2024-10-06 23:27:31 +0900

    update: ローカルストレージに保存

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
 import { locales } from "@blocknote/core";
+import { Block, BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
-import { useCreateBlockNote } from "@blocknote/react";
+import { useEffect, useMemo, useState } from "react";
+
+async function saveToStorage(jsonBlocks: Block[]) {
+    // ローカルストレージにコンテンツを保存する。
+    // 場合に応じてAPIやデータベースに保存するように変更する。
+    localStorage.setItem("editorContent", JSON.stringify(jsonBlocks));
+}
+
+async function loadFromStorage() {
+    // 前のエディターの内容を取得する。
+    const storageString = localStorage.getItem("editorContent");
+    return storageString ? (JSON.parse(storageString) as PartialBlock[]) : undefined;
+}
 
 export default function App() {
-    // Creates a new editor instance.
-    const editor = useCreateBlockNote({
-        dictionary: locales.ja,
-    });
+    const [initialContent, setInitialContent] = useState<PartialBlock[] | undefined | "loading">("loading");
 
-    // Renders the editor instance using a React component.
-    return <BlockNoteView editor={editor} />;
+    // 前のエディターの内容をロードする。
+    useEffect(() => {
+        loadFromStorage().then((content) => {
+            setInitialContent(content);
+        });
+    }, []);
+
+    // 初期コンテンツがロードされるまでエディターの作成を遅らせるようにするため、
+    // useCreateBlockNoteを使わずにuseMemo + createBlockNoteEditorを使う。
+    const editor = useMemo(() => {
+        if (initialContent === "loading") {
+            return undefined;
+        }
+        return BlockNoteEditor.create({
+            initialContent: initialContent,
+            dictionary: locales.ja,
+        });
+    }, [initialContent]);
+
+    if (editor === undefined) {
+        return "Loading content...";
+    }
+
+    return (
+        <BlockNoteView
+            editor={editor}
+            onChange={() => {
+                saveToStorage(editor.document);
+            }}
+        />
+    );
 }
